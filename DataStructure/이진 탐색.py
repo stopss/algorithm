8e917f2870380013c39c23f7253977932f0cee40
jieun <stopss417@gmail.com>
2022-05-28 15:46:14 +0900

    algorithm

--- DataStructure/이진 탐색.py
+++ DataStructure/이진 탐색.py
@@ -1,55 +1,85 @@
-import bisect
+# import bisect
+#
+#
+# def binary_search1(target, data):
+#     data.sort()
+#     left = 0
+#     right = len(data) - 1
+#
+#     while left <= right :
+#         mid = (left + right) // 2
+#
+#         if data[mid] == target:
+#             return mid
+#         elif data[mid] < target:
+#             left = mid + 1
+#         else:
+#             right = mid - 1
+#
+#     return None
+#
+# # 재귀적 구현
+# def binary_search2(target, data, left, right):
+#     data.sort()
+#     if left > right:
+#         return None
+#
+#     mid = (left + right) // 2
+#     if data[mid] == target:
+#         return mid
+#     elif data[mid] < target:
+#         left = mid + 1
+#     else:
+#         right = mid - 1
+#
+#     return binary_search2(target, data, left, right)
+#
+#
+# # 내장 라이브러리 사용
+# def binary_search3(target, data):
+#     data.sort()
+#     idx = bisect.bisect_left(data, target)
+#     if idx < len(data) and data[idx] == target:
+#         return idx
+#     else:
+#         return None
+#
+#
+# target = 25
+# data = [31, 25, 7, 81, 70, 11, 43, 56, 19, 63, 21, 98]
+# right = len(data) - 1
+# print(binary_search1(target, data))
+# print(binary_search2(target, data, 0, right))
+# print(binary_search3(target, data))
 
 
-def binary_search1(target, data):
-    data.sort()
+
+# 부품 찾기
+n = int(input())
+n_arr = list(map(int, input().split()))
+
+m = int(input())
+m_arr = list(map(int, input().split()))
+
+def binary_search(n_arr, target):
+    n_arr.sort()
+
     left = 0
-    right = len(data) - 1
-
-    while left <= right :
+    right = n
+    while left <= right:
         mid = (left + right) // 2
-
-        if data[mid] == target:
+        if n_arr[mid] == target:
             return mid
-        elif data[mid] < target:
+        elif n_arr[mid] < target:
             left = mid + 1
-        else:
+        elif:
             right = mid - 1
-
     return None
 
-# 재귀적 구현
-def binary_search2(target, data, start, end):
-    data.sort()
-    if start > end:
-        return None
-
-    mid = (start + end) // 2
-    if data[mid] == target:
-        return mid
-    elif data[mid] < target:
-        start = mid + 1
+for e in m_arr:
+    result = binary_search(n_arr, e)
+    if result != None:
+        print('yes', end=' ')
     else:
-        end = mid - 1
-
-    return binary_search2(target, data, start, end)
-
-
-# 내장 라이브러리 사용
-def binary_search3(target, data):
-    data.sort()
-    idx = bisect.bisect_left(data, target)
-    if idx < len(data) and data[idx] == target:
-        return idx
-    else:
-        return None
-
-
-target = 25
-data = [31, 25, 7, 81, 70, 11, 43, 56, 19, 63, 21, 98]
-end = len(data) - 1
-# print(data.sort())
-print(binary_search1(target, data))
-print(binary_search2(target, data, 0, end))
-print(binary_search3(target, data))
+        print('no', end=' ')
 
